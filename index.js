8ea72ccf5c0e35fc30510828757626d85d1e25e0
Shant Dashjian <shantgdashjian@gmail.com>
2024-03-20 20:50:42 +0400

    Add GET all endpoint

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from 'express'
 import cors from 'cors'
 
+import { readAll } from './repository.js'
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
 const PORT = process.env.PORT | 3000
+
+app.get('/api/synonym', (req, res) => {
+    return res.status(200).json(readAll())
+})
 
 app.listen(PORT, () => {
     console.log('Listening to port ' + PORT)
